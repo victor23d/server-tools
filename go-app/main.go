264e363133c769fb55ca24634cd1e4439359c6aa
victor23d <cptdm2@gmail.com>
2019-08-15 16:27:48 +0800

    echo ip

--- go-app/main.go
+++ go-app/main.go
@@ -12,8 +12,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println(host)
 	fmt.Fprintf(w, host)
 
-	log.Printf("%s", r.URL.Path[1:])
-	fmt.Fprintf(w, "%s", r.URL.Path[1:])
+	fmt.Fprintf(w, "%s", "\n")
+	log.Printf("r.URL.Path: %s\n", r.URL.Path[1:])
+	fmt.Fprintf(w, "r.URL.Path: %s\n", r.URL.Path[1:])
+	// fmt.Fprintf(w, "%s", "\n")
+	GetIP(w, r)
+}
+
+func GetIP(w http.ResponseWriter, r *http.Request) {
+
+	XRealIP := r.Header.Get("X-Real-Ip")
+	fmt.Fprintf(w, "X-Real-Ip: "+XRealIP+"\n")
+
+	XForwardFor := r.Header.Get("X-Forwarded-For")
+	fmt.Fprintf(w, "X-Forwarded-For: "+XForwardFor+"\n")
+
+	RemoteAddr := r.RemoteAddr
+	fmt.Fprintf(w, "RemoteAddr: "+RemoteAddr+"\n")
 }
 
 func main() {
