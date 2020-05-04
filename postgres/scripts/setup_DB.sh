b3e2202cdbd71570d9a25d0aaae505ca1886cb00
victor23d <cptdm2@gmail.com>
2020-05-04 12:23:58 +0800

    modified:   postgres/scripts/setup_DB.sh

--- postgres/scripts/setup_DB.sh
+++ postgres/scripts/setup_DB.sh
@@ -1,10 +1,10 @@
 # docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=123 postgres:12-alpine
-docker run  -p 5432:5432 \
+docker run -d -p 5432:5432 \
     -e POSTGRES_PASSWORD=123 \
     -e POSTGRES_USER=sandbox_admin \
     -e POSTGRES_DB=sandbox_db1 \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
-    -v $PWD/../pgdata/:/var/lib/postgresql/data \
+    -v ~/pgdata/:/var/lib/postgresql/data \
     postgres:12-alpine
 
 # sleep 5
