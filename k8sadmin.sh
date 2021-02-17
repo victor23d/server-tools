a653a7210e980841564391d040cd92ce34d21065
victor23d <cptdm2@gmail.com>
2021-02-18 00:13:19 +0800

    new file:   k8sadmin.sh

---
+++ k8sadmin.sh
@@ -0,0 +1,3 @@
+kubectl create serviceaccount k8sadmin -n kube-system
+kubectl create clusterrolebinding k8sadmin --clusterrole=cluster-admin --serviceaccount=kube-system:k8sadmin
+kubectl -n kube-system describe secret $(sudo kubectl -n kube-system get secret | (grep k8sadmin || echo "$_") | awk '{print $1}') | grep token: | awk '{print $2}'
